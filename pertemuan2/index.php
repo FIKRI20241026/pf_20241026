e0209966ccbae2d531c33618b711d69726c8bff6
unknown <fikrijati442@gmail.com>
2022-11-03 10:37:31 +0800

    File Project Baru 03/11/22

---
+++ pertemuan2/index.php
@@ -0,0 +1,31 @@
+<?php
+        //standar output di php
+    //echo, print, print-f, var_dump
+
+    $nama = "bung fikri";
+    echo "Hello, Nama saya $nama";
+
+    //Operator
+    //aritmatika
+    //* / + - %
+    $x = 10;
+    $y = 20;
+    echo $x + $y;
+
+    //penggabungan string /concatenation /concat
+    //.
+    $nama_depan = "bung";
+    $nama_belakang = "fikri";
+    echo $nama_depan . " " . $nama_belakang;
+
+    //assignment
+    //=,+=,-=,*=,/=,%,.=
+    $x = 1;
+    $x /= 5;
+    echo $x;
+    $nama .= $nama_depan;
+    $nama .= " ";
+    $nama .= $nama_belakang;
+    echo $nama;
+
+?>
